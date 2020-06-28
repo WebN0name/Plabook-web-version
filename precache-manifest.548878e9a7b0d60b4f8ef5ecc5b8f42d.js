7f2183a14ad4e90bf18f0321f56b5f74f242b9b1
Haswell <Haswelvc@yandex.ru>
2020-06-28 20:01:09 +0700

    Updates

---
+++ precache-manifest.548878e9a7b0d60b4f8ef5ecc5b8f42d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cd68bacbf7d3022924aa0e389f455c",
+    "url": "/Plabook-web-version/index.html"
+  },
+  {
+    "revision": "69cf2a498b51c62d34a1",
+    "url": "/Plabook-web-version/static/css/main.6f644f59.chunk.css"
+  },
+  {
+    "revision": "463e7936becf76ec4dfc",
+    "url": "/Plabook-web-version/static/js/2.f8766fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Plabook-web-version/static/js/2.f8766fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cf2a498b51c62d34a1",
+    "url": "/Plabook-web-version/static/js/main.75180e3a.chunk.js"
+  },
+  {
+    "revision": "1d5ad040fcd86ee7a16c",
+    "url": "/Plabook-web-version/static/js/runtime-main.05b1261c.js"
+  },
+  {
+    "revision": "5c5bb4c154542b8e2f107db9b5c65b4c",
+    "url": "/Plabook-web-version/static/media/Bird.5c5bb4c1.png"
+  }
+]);
